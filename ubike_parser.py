db4a5752b9b19beffaeb0dca84694af9f3c0cb1c
lyoxin0o0 <wanthappy0o0@gmail.com>
2026-08-21 15:18:09 +0800

    新增資料狀態顯示

--- ubike_parser.py
+++ ubike_parser.py
@@ -86,3 +86,7 @@ else:
 print("YouBike 資料更新完成！")
 print("抓取時間：", datetime.now())
 print("本次資料筆數：", len(df))
+
+history_df = pd.read_csv("data/ubike_history.csv")
+print("目前歷史資料總筆數：", len(history_df))
+print("最新抓取時間：", history_df["抓取時間"].iloc[-1])
